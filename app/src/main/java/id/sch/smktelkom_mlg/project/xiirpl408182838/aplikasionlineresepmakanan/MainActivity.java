4035c25cf14e7e63750839be162f81e54cdf156c
keniamalia <keniamalia1@gmail.com>
2016-11-01 08:40:52 +0700

    Proses Login Menggunakan Akun Google

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl408182838/aplikasionlineresepmakanan/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl408182838/aplikasionlineresepmakanan/MainActivity.java
@@ -1,5 +1,6 @@
 package id.sch.smktelkom_mlg.project.xiirpl408182838.aplikasionlineresepmakanan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
     }
 }
